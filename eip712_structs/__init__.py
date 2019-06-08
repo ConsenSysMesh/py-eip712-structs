ef5e844dc5586690981fd30a7de3184f016eae4b
AJ Grubbs <anthony.grubbs@consensys.net>
2019-06-07 23:10:12 -0500

    Bump version to 1.0.0

--- eip712_structs/__init__.py
+++ eip712_structs/__init__.py
@@ -3,4 +3,4 @@ from eip712_structs.struct import EIP712Struct
 from eip712_structs.types import Address, Array, Boolean, Bytes, Int, String, Uint
 
 name = 'eip712-structs'
-version = '0.1.6'
+version = '1.0.0'
